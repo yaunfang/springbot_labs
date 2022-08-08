d87e8b7e8d02721aa2f677aa3fda7629483e26a7
袁 <13387908010@163.com>
2022-08-08 15:01:31 +0800

    security 提交

---
+++ spring-security/springsecurity-demo-role/src/main/java/com/yuan/springsecuritydemorole/SpringsecurityDemoRoleApplication.java
@@ -0,0 +1,13 @@
+package com.yuan.springsecuritydemorole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityDemoRoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityDemoRoleApplication.class, args);
+    }
+
+}
